36b88ab21771b5bd511e96fbeb234e38e7adb835
Alex Rubinshteyn <alexander.rubinshteyn@student.kit.edu>
2018-04-23 22:18:06 +0200

    add helper class 'hotspot_interpreter' and the tests for it

---
+++ sfgad/modules/feature/helper/hotspot_interpreter.py
@@ -0,0 +1,287 @@
+import pandas as pd
+import numpy as np
+from collections import defaultdict, deque
+import copy
+
+
+class HotSpotInterpreter:
+    """
+    The HotSpot-interpreter is a helper class, which interprets a given edge_frame and stores all relevant information
+    about the available vertices and edges.
+    """
+
+    def __init__(self, decay_lambda, half_life):
+
+        # decay constant
+        self.decay_lambda = decay_lambda
+        # half life of an edge
+        self.half_life = half_life
+
+        # mapping of nodes to numbers
+        self.ids = {}
+        # reverse mapping of numbers to nodes names
+        self.inv_ids = {}
+
+        # a dictionary, which contains a tuple (t; f) for each edge: the time point t when the edge frequency was
+        # updated, and the frequency f
+        self.frequencies = {}
+        # a boolean dictionary, which memorizes whether an edge was already updated in the current time step
+        self.edge_updated = defaultdict(bool)
+
+        # a dictionary, which contains a list for each node: the ids of the neighbors
+        self.neighbors = defaultdict(list)
+
+        # a dictionary, which contains the information about the 2 last interpret()-calls (t; n_nodes; f; neighbors):
+        # the timestamp t, the number of nodes (n_nodes), the frequencies f, and the neighbors (neighbors)
+        self.fit_buffer = deque(maxlen=2)
+        self.fit_buffer.append((pd.Timestamp.min, 0, {}, defaultdict(list)))
+
+    def interpret(self, df_edges):
+        """
+        Interprets the given edge_frame by: registering new nodes; updating the node neighbors; updating the edge
+        frequencies; and updating the fit buffer.
+        :param df_edges: The edge_frame to interpret.
+        :return: the unique node names in df_edges, and the current time stamp
+        """
+
+        current_time, n_nodes, _, _ = self.fit_buffer[-1]
+
+        # registration phase of nodes
+        unique_nodes = list(pd.unique(df_edges[['SRC_NAME', 'DST_NAME']].values.ravel()))
+        for node_name in unique_nodes:
+            if node_name not in self.ids:
+                n_nodes = self.register_node(node_name, n_nodes)
+
+        # update the current time
+        if not df_edges.empty:
+            assert (min(df_edges["TIMESTAMP"]) - current_time).total_seconds() >= 0
+            current_time = max(df_edges["TIMESTAMP"])
+
+        # interpret the new edges
+        self.interpret_df(df_edges, current_time)
+
+        # update the fit buffer
+        self.fit_buffer.append((current_time, n_nodes, copy.deepcopy(self.frequencies), copy.deepcopy(self.neighbors)))
+        # reset the edge occurrences of the current time step
+        self.edge_updated = defaultdict(bool)
+
+        return current_time, unique_nodes
+
+    ### HELPER METHODS
+
+    def interpret_df(self, df_edges, current_time):
+        """
+        Interprets the given edge_frame by interpreting each edge.
+        :param df_edges: The edge_frame to interpret.
+        :param current_time: The current time stamp.
+        """
+
+        iterator = zip(df_edges["SRC_NAME"], df_edges["DST_NAME"])
+        for s, d in iterator:
+            self.interpret_edge(s, d, current_time)
+
+    def interpret_edge(self, s, d, t):
+        """
+        Interprets the given edge by updating the node neighbors and the edge frequency.
+        :param s: The source node (name) of the edge.
+        :param d: The distance node (name) of the edge.
+        :param t: The current time stamp.
+        """
+
+        # map source and destination nodes to their ids
+        s, d = self.ids[s], self.ids[d]
+        # make sure that source id is smaller than destination id
+        if s > d:
+            s, d = d, s
+        edge = (s, d)
+
+        # update the neighbors
+        self.update_neighbor(s, d)
+        self.update_neighbor(d, s)
+
+        # update the occurrences with new timestamp and new frequency
+        # if the edge has already occurred in current time window, then overwrite the last entry
+        if not self.edge_updated[edge]:
+            self.frequencies[edge] = (t, self.update_weighted_frequency(edge, t))
+            self.edge_updated[edge] = True
+        else:
+            t, freq = self.frequencies[edge]
+            self.frequencies[edge] = (t, freq + 1)
+
+    def update_weighted_frequency(self, edge, t):
+        """
+        Updates the frequency of the given edge by considering the occurrence at the time t.
+        :param edge: The edge, which frequency to update.
+        :param t: The time stamp at which the edge arrived (again).
+        """
+
+        freq_new = 0
+
+        # consider the previous frequency of this edge
+        if edge in self.frequencies:
+            t_old, freq_old = self.frequencies[edge]
+            freq_new = freq_old * 2 ** (-self.decay_lambda * (t - t_old).total_seconds())
+
+        # consider the new arrival of the edge
+        freq_new += 1
+
+        return freq_new
+
+    def register_node(self, node_name, n_nodes):
+        """
+        Records the new node by assigning an ID to it.
+        :param node_name: The new node to record.
+        :param n_nodes: The current amount of nodes.
+        :return: the updated amount of nodes.
+        """
+
+        self.ids[node_name] = n_nodes
+        self.inv_ids[n_nodes] = node_name
+        n_nodes += 1
+
+        return n_nodes
+
+    def update_neighbor(self, node_id, neighbor_id):
+        """
+        Updates the neighbor of the given node.
+        :param node_id: The given node.
+        :param neighbor_id: The neighbor to update.
+        """
+
+        if neighbor_id not in self.neighbors[node_id]:
+            self.neighbors[node_id].append(neighbor_id)
+
+    def turn_back_time(self):
+        """
+        Resets all the updates of the interpreter of the last time step by going one interpret_df()-call back
+        """
+
+        t_now, n_nodes_now, _, _ = self.fit_buffer[-1]
+        t_prev_fit, n_nodes_prev_fit, frequencies_prev_fit, neighbors_prev_fit = self.fit_buffer[-2]
+
+        # set back the ids and inv_ids (by deleting the newly arrived nodes)
+        nodes_to_delete = range(n_nodes_prev_fit, n_nodes_now)
+        for node_id in nodes_to_delete:
+            node_name = self.inv_ids[node_id]
+            del self.inv_ids[node_id]
+            del self.ids[node_name]
+
+        # set back the neighbors
+        self.neighbors = neighbors_prev_fit
+
+        # set back the frequencies
+        self.frequencies = frequencies_prev_fit
+
+        # set back the fit-buffer
+        del self.fit_buffer[-1]
+
+    ### GETTER METHODS
+
+    def get_freq(self, edge):
+        """
+        Returns the edge frequency of the given edge at the current time.
+        :param edge: The given edge.
+        :return: The edge frequency at the current time.
+        """
+
+        if edge not in self.frequencies:
+            raise ValueError("Error! The given edge is unknown!")
+
+        t, freq = self.frequencies[edge]
+        cur_time = self.get_current_time()
+
+        # update the frequency to the current time
+        if cur_time > t:
+            freq *= 2 ** (-self.decay_lambda * (cur_time - t).total_seconds())
+            self.frequencies[edge] = (cur_time, freq)
+
+        return freq
+
+    def get_node_id(self, node_name):
+        """
+        Returns the id of the given node characterized by its name.
+        :param node_name: The given node.
+        :return: The id of the given.
+        """
+
+        # Exception, if node_id is unknown
+        if node_name not in self.ids:
+            raise ValueError("Error! The given node_name is unknown!")
+
+        return self.ids[node_name]
+
+    def get_neighbor_id(self, node_id, neighbor_idx):
+        """
+        Returns the id of the neighbor at the given index.
+        :param node_id: The given node.
+        :param neighbor_idx: The index of the neighbor.
+        :return: The id of the neighbor.
+        """
+        if neighbor_idx >= len(self.neighbors[node_id]):
+            raise ValueError("Error! The given neighbor index is invalid!")
+
+        return self.neighbors[node_id][neighbor_idx]
+
+    def get_current_time(self):
+        """
+        Returns the current time of the interpreter.
+        :return: The current time.
+        """
+
+        t, _, _, _ = self.fit_buffer[-1]
+
+        return t
+
+    def get_n_neighbors(self, node_id):
+        """
+        Returns the number of neighbors of the given node.
+        :param node_id: The given node.
+        :return: The number of neighbors.
+        """
+
+        # Exception, if node_id is unknown
+        if node_id not in self.neighbors:
+            raise ValueError("Error! The given node_id is unknown!")
+
+        return len(self.neighbors[node_id])
+
+    def get_neighbors(self, node_id):
+        """
+        Returns the neighbors of the given node.
+        :param node_id: The given node.
+        :return: A list with the neighbor ids.
+        """
+
+        # Exception, if node_id is unknown
+        if node_id not in self.neighbors:
+            raise ValueError("Error! The given node_id is unknown!")
+
+        return self.neighbors[node_id]
+
+
+    #def del_neighbor(self, node_id, neighbor_id):
+    #    neighbor_index = self.get_neighbor_idx(node_id, neighbor_id)
+
+    #    if not np.isnan(neighbor_index):
+    #        del self.neighbors[node_id][neighbor_index]
+    #        del self.neighbors_time[node_id][neighbor_index]
+
+    #def get_neighbor_idx(self, node_id, neighbor_id):
+
+    #    neighbor_index = np.nan
+
+    #    if len(self.neighbors[node_id]) > 0:
+    #        neighbors = self.neighbors[node_id]
+
+    #        if neighbor_id in neighbors:
+    #            neighbor_index = neighbors.index(neighbor_id)
+
+    #    return neighbor_index
+
+    #def get_node_name(self, node_id):
+
+        # Exception, if node_id is unknown
+    #    if node_id not in self.inv_ids:
+    #        raise ValueError("Error! The given node_id is unknown!")
+
+    #    return self.inv_ids[node_id]
---
+++ sfgad/test/feature/test_hotspot_interpreter.py
@@ -0,0 +1,173 @@
+import unittest
+import pandas as pd
+import copy
+
+from collections import deque
+from sfgad.modules.feature.helper.hotspot_interpreter import HotSpotInterpreter
+
+
+class TestHotSpotInterpreter(unittest.TestCase):
+
+    def setUp(self):
+
+        self.df_1 = pd.DataFrame({'TIMESTAMP': ['2018-01-01 00:00:00', '2018-01-01 00:00:01', '2018-01-01 00:00:05'],
+                             'SRC_NAME': ['A', 'A', 'B'],
+                             'SRC_TYPE': ['NODE', 'NODE', 'NODE'],
+                             'DST_NAME': ['B', 'C', 'C'],
+                             'DST_TYPE': ['NODE', 'NODE', 'NODE']})
+        self.df_1['TIMESTAMP'] = pd.to_datetime(self.df_1['TIMESTAMP'])
+
+        self.df_2 = pd.DataFrame({'TIMESTAMP': ['2018-01-01 00:00:10', '2018-01-01 00:00:12', '2018-01-01 00:00:14'],
+                             'SRC_NAME': ['A', 'A', 'C'],
+                             'SRC_TYPE': ['NODE', 'NODE', 'NODE'],
+                             'DST_NAME': ['B', 'C', 'D'],
+                             'DST_TYPE': ['NODE', 'NODE', 'NODE']})
+        self.df_2['TIMESTAMP'] = pd.to_datetime(self.df_2['TIMESTAMP'])
+
+        self.window_size = 10
+        self.half_life = 2
+        self.decay_lambda = 1 / (self.half_life * self.window_size)
+
+        self.interpreter = HotSpotInterpreter(self.decay_lambda, self.half_life)
+
+    def test_init(self):
+        self.assertEqual(self.interpreter.decay_lambda, self.decay_lambda)
+        self.assertEqual(self.interpreter.half_life, self.half_life)
+        self.assertEqual(len(self.interpreter.fit_buffer), 1)
+        self.assertEqual(self.interpreter.ids, {})
+        self.assertEqual(self.interpreter.inv_ids, {})
+        self.assertEqual(self.interpreter.frequencies, {})
+        self.assertEqual(self.interpreter.edge_updated, {})
+        self.assertEqual(self.interpreter.neighbors, {})
+        self.assertEqual(self.interpreter.fit_buffer[-1], (pd.Timestamp.min, 0, {}, {}))
+
+    def test_interpret(self):
+
+        self.assertEqual(self.interpreter.interpret(self.df_1), (pd.Timestamp('2018-01-01 00:00:05'), ['A', 'B', 'C']))
+
+        fit_buffer = deque([(pd.Timestamp('1677-09-21 00:12:43.145225'), 0, {}, {}),
+               (pd.Timestamp('2018-01-01 00:00:05'),
+                3,
+                {(0, 1): (pd.Timestamp('2018-01-01 00:00:05'), 1),
+                 (0, 2): (pd.Timestamp('2018-01-01 00:00:05'), 1),
+                 (1, 2): (pd.Timestamp('2018-01-01 00:00:05'), 1)},
+                {0: [1, 2], 1: [0, 2], 2: [0, 1]})])
+
+        self.assertEqual(self.interpreter.fit_buffer, fit_buffer)
+
+        self.assertEqual(self.interpreter.edge_updated, {})
+
+        # verify the assertion
+        self.assertRaises(AssertionError, self.interpreter.interpret, self.df_1)
+
+    def test_interpret_edge(self):
+        # 1. occurrence in a time window
+        self.interpreter.register_node("A", 0)
+        self.interpreter.register_node("B", 1)
+        self.interpreter.interpret_edge("B", "A", pd.Timestamp('2018-01-01 00:00:00'))
+
+        self.assertEqual(self.interpreter.get_neighbors(0), [1])
+        self.assertEqual(self.interpreter.get_neighbors(1), [0])
+
+        self.assertEqual(self.interpreter.frequencies[(0, 1)], (pd.Timestamp('2018-01-01 00:00:00'), 1))
+
+        # 2. occurrence in a time window
+        self.interpreter.interpret_edge("A", "B", pd.Timestamp('2018-01-01 00:00:00'))
+        self.assertEqual(self.interpreter.frequencies[(0, 1)], (pd.Timestamp('2018-01-01 00:00:00'), 2))
+
+    def test_update_weighted_frequency(self):
+        # a new edge
+        self.assertEqual(self.interpreter.update_weighted_frequency((0, 1), pd.Timestamp('2018-01-01 00:00:00')), 1)
+
+        # a new arrival of an edge
+        self.interpreter.frequencies[(0, 1)] = (pd.Timestamp('2018-01-01 00:00:00'), 1)
+        self.assertEqual(self.interpreter.update_weighted_frequency((0, 1), pd.Timestamp('2018-01-01 00:00:05')),
+                         1 * 2 ** (-self.decay_lambda * 5) + 1)
+
+    def register_node(self):
+        self.assertEqual(self.interpreter.register_node("A", 3), 4)
+        self.assertEqual(self.interpreter.ids["A"], 3)
+        self.assertEqual(self.interpreter.inv_ids[3], "A")
+
+    def test_update_neighbor(self):
+        self.interpreter.neighbors[0] = [1, 2]
+
+        # try to add an existing neighbor
+        self.interpreter.update_neighbor(0, 1)
+        self.assertEqual(self.interpreter.get_neighbors(0), [1, 2])
+
+        # add a new neighbor
+        self.interpreter.update_neighbor(0, 3)
+        self.assertEqual(self.interpreter.get_neighbors(0), [1, 2, 3])
+
+    def test_turn_back_time(self):
+
+        # let the interpreter intepret the 1. dataframe and make a copy
+        self.interpreter.interpret(self.df_1)
+        interpreter_copy = copy.deepcopy(self.interpreter)
+
+        # let the interpreter intepret the 2. dataframe and turn back time
+        self.interpreter.interpret(self.df_2)
+        self.interpreter.turn_back_time()
+
+        # assert that the interpreter returned to the state after the 1. interpret call
+        self.assertEqual(self.interpreter.ids, interpreter_copy.ids)
+        self.assertEqual(self.interpreter.inv_ids, interpreter_copy.inv_ids)
+        self.assertEqual(self.interpreter.frequencies, interpreter_copy.frequencies)
+        self.assertEqual(self.interpreter.neighbors, interpreter_copy.neighbors)
+        self.assertEqual(self.interpreter.fit_buffer[-1], interpreter_copy.fit_buffer[-1])
+
+
+    def test_get_freq(self):
+        # edge (0, 1) is unknown
+        self.assertRaises(ValueError, self.interpreter.get_freq, (0, 1))
+
+        # edge (0, 1) was added recently
+        self.interpreter.frequencies[(0, 1)] = (pd.Timestamp.min, 1.23)
+        self.assertEqual(self.interpreter.get_freq((0, 1)), 1.23)
+
+    def test_get_node_id(self):
+        # node with the name "A" is unknown
+        self.assertRaises(ValueError, self.interpreter.get_node_id, "A")
+
+        # node with the name "A" was added recently and has the id 0
+        self.interpreter.register_node("A", 0)
+        self.assertEqual(self.interpreter.get_node_id("A"), 0)
+
+    def test_get_neighbor_id(self):
+        self.interpreter.neighbors[1].append(2)
+
+        # node 1 has no neighbor at index 1
+        self.assertRaises(ValueError, self.interpreter.get_neighbor_id, 1, 1)
+
+        # now node 1 has a neighbor at index 1
+        self.interpreter.neighbors[1].append(3)
+        self.assertEqual(self.interpreter.get_neighbor_id(1, 1), 3)
+
+    def test_get_current_time(self):
+        # interpreter is empty
+        self.assertEqual(self.interpreter.get_current_time(), pd.Timestamp.min)
+
+        # interpreter had 1 interpret() call
+        self.interpreter.interpret(self.df_1)
+        self.assertEqual(self.interpreter.get_current_time(), pd.Timestamp('2018-01-01 00:00:05'))
+
+    def test_get_n_neighbors(self):
+        # node 1 has no neighbors
+        self.assertRaises(ValueError, self.interpreter.get_n_neighbors, 1)
+
+        # node 1 has 1 neighbor
+        self.interpreter.neighbors[1].append(2)
+        self.assertEqual(self.interpreter.get_n_neighbors(1), 1)
+
+    def test_get_neighbors(self):
+        # node 1 has no neighbors
+        self.assertRaises(ValueError, self.interpreter.get_n_neighbors, 1)
+
+        # node 1 has 1 neighbor
+        self.interpreter.neighbors[1].append(2)
+        self.assertEqual(self.interpreter.get_neighbors(1), [2])
+
+
+if __name__ == '__main__':
+    unittest.main()
