7b810138ce346de35aed7a16809ec94a5a601406
Alex Rubinshteyn <alexander.rubinshteyn@student.kit.edu>
2018-04-23 23:15:47 +0200

    add feature VertexDegree with tests

---
+++ sfgad/modules/feature/vertex_degree.py
@@ -0,0 +1,36 @@
+from .feature import Feature
+
+
+class VertexDegree(Feature):
+    """
+    The feature VertexDegree of a single vertex is defined as the number of occurrences as either SRC or DST node in all
+    the edges of the current time window
+    """
+
+    def __init__(self):
+        self.names = ['VertexDegree']
+
+    def process_vertices(self, df_edges, n_jobs, update_activity=True):
+        """
+        Iterates over the current data frame and calculates for each vertex the vertex degree.
+        :param df_edges: The data frame to process.
+        :param n_jobs: The number of cores that are supported for multiprocessing.
+        :param update_activity: True, if the feature should consider the new edges for future computations (if needed),
+            false otherwise.
+        :return a data frame with the columns ['name', 'VertexDegree'] and the calculated vertex degree for all vertices
+            in the given df_edges.
+        """
+
+        src_counts = df_edges.SRC_NAME.value_counts()
+        dst_counts = df_edges.DST_NAME.value_counts()
+
+        counts = src_counts.add(dst_counts, fill_value=0).astype('int64')
+
+        count_df = counts.to_frame().reset_index()
+        count_df.columns = ['name'] + self.names
+
+        return count_df
+
+    def compute(self, node_name, t):
+        # Not needed here, since this feature is to simple for multiprocessing
+        pass
---
+++ sfgad/test/feature/test_vertex_degree.py
@@ -0,0 +1,27 @@
+from unittest import TestCase
+import pandas as pd
+from pandas.util.testing import assert_frame_equal
+
+from sfgad.modules.feature.vertex_degree import VertexDegree
+
+
+class TestVertexDegree(TestCase):
+
+    def setUp(self):
+        self.df = pd.DataFrame({'TIMESTAMP': ['2018-01-01 00:00:00', '2018-01-01 00:00:01', '2018-01-01 00:00:05'],
+                                  'SRC_NAME': ['A', 'A', 'B'],
+                                  'SRC_TYPE': ['NODE', 'NODE', 'NODE'],
+                                  'DST_NAME': ['B', 'C', 'C'],
+                                  'DST_TYPE': ['NODE', 'NODE', 'NODE']})
+        self.df['TIMESTAMP'] = pd.to_datetime(self.df['TIMESTAMP'])
+
+        self.feature = VertexDegree()
+
+    def test_init(self):
+        self.assertEqual(self.feature.names, ['VertexDegree'])
+
+    def test_process_vertices(self):
+        # test the calculation of vertex degree on the example data frame 'df'
+        result_df = pd.DataFrame(data={'name': ['A', 'B', 'C'], 'VertexDegree': [2, 2, 2]},
+                                 columns = ['name', 'VertexDegree'])
+        assert_frame_equal(self.feature.process_vertices(self.df, 1), result_df)
