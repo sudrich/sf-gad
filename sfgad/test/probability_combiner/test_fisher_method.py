cb64439519a1c745d62e6febf4bb3d8641d8a0b0
Alex Rubinshteyn <alexander.rubinshteyn@student.kit.edu>
2018-05-10 23:56:32 +0200

    add tests for all existing probability combiner

---
+++ sfgad/test/probability_combiner/test_fisher_method.py
@@ -0,0 +1,32 @@
+from unittest import TestCase
+from sfgad.modules.probability_combiner.fisher_method import FisherMethod
+
+
+class TestFisherMethod(TestCase):
+
+    def setUp(self):
+        self.combiner = FisherMethod()
+
+    def test_combine_output(self):
+        p_values = [0.21, 0.12, 0.021, 0.15, 0.067]
+
+        # test the right output for position 2
+        self.assertAlmostEqual(self.combiner.combine(p_values), 0.0068, delta=0.0001)
+
+    def test_combine_empty_list(self):
+        p_values = []
+
+        # expect a value error
+        self.assertRaises(ValueError, self.combiner.combine, p_values)
+
+    def test_combine_type_list(self):
+        p_values = 42
+
+        # expect an assertion error
+        self.assertRaises(AssertionError, self.combiner.combine, p_values)
+
+    def test_combine_type_elements(self):
+        p_values = [0.21, 0.12, 'A', 0.15, 0.067]
+
+        # expect an assertion error
+        self.assertRaises(ValueError, self.combiner.combine, p_values)
---
+++ sfgad/test/probability_combiner/test_selected_feature_probability.py
@@ -0,0 +1,40 @@
+from unittest import TestCase
+from sfgad.modules.probability_combiner.selected_feature_probability import SelectedFeatureProbability
+
+
+class TestSelectedFeatureProbability(TestCase):
+
+    def setUp(self):
+        self.combiner = SelectedFeatureProbability(feature_position=2)
+
+    def test_combine_output(self):
+        p_values = [0.21, 0.12, 0.021, 0.15, 0.067]
+
+        # test the right output for position 2
+        self.assertEqual(self.combiner.combine(p_values), p_values[2])
+
+    def test_combine_empty_list(self):
+        p_values = []
+
+        # expect a value error
+        self.assertRaises(ValueError, self.combiner.combine, p_values)
+
+    def test_combine_type_list(self):
+        p_values = 42
+
+        # expect an assertion error
+        self.assertRaises(AssertionError, self.combiner.combine, p_values)
+
+    def test_combine_type_elements(self):
+        p_values = [0.21, 0.12, 'A', 0.15, 0.067]
+
+        # expect an assertion error
+        self.assertRaises(ValueError, self.combiner.combine, p_values)
+
+    def test_invalid_position(self):
+        combiner = SelectedFeatureProbability(feature_position=5)
+        p_values = [0.21, 0.12, 0.021, 0.15, 0.067]
+
+        # expect an assertion error
+        self.assertRaises(ValueError, combiner.combine, p_values)
+
