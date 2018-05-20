84bbf64abc5c8470d69056d3ef809e19204beac2
Alex Rubinshteyn <alexander.rubinshteyn@student.kit.edu>
2018-05-20 16:33:31 +0200

    add uniform probability estimator

--- sfgad/modules/probability_estimator/probability_estimator.py
+++ sfgad/modules/probability_estimator/probability_estimator.py
@@ -7,8 +7,8 @@ class ProbabilityEstimator(metaclass=abc.ABCMeta):
     def estimate(self, vertex_name, features_values, reference_features_values, weights):
         """
         Takes a vertex and a reference to the database.
-        Returns a list of p_values with a p_value for each given feature value in features_df. These p_values are
-        calculated based on the given reference_feature_values.
+        Returns a list of p_values with a p_value for each given feature value in features_values. These p_values are
+        calculated based on the given reference_features_values.
         :param vertex_name: the name of the vertex.
         :param features_values: Dataframe with values of features.
         :param reference_features_values: List of tuples of windows and the corresponding feature values.
