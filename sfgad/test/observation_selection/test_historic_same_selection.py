c63b04497825bf93b0582338a680d7cc3c1992ae
Alex Rubinshteyn <alexander.rubinshteyn@student.kit.edu>
2018-05-21 13:23:05 +0200

    add templates for observation_selection tests

---
+++ sfgad/test/observation_selection/test_historic_same_selection.py
@@ -0,0 +1,8 @@
+from unittest import TestCase
+from sfgad.modules.observation_selection.historic_same_selection import HistoricSameSelection
+
+
+class TestHistoricSameSelection(TestCase):
+
+    def test_gather(self):
+        self.fail()
---
+++ sfgad/test/observation_selection/test_historic_similar_selection.py
@@ -0,0 +1,8 @@
+from unittest import TestCase
+from sfgad.modules.observation_selection.historic_similar_selection import HistoricSimilarSelection
+
+
+class TestHistoricSimilarSelection(TestCase):
+
+    def test_gather(self):
+        self.fail()
