dc1b25f0570ea3244b735a05037132c3fa4f9053
Alex Rubinshteyn <alexander.rubinshteyn@student.kit.edu>
2018-05-21 13:16:54 +0200

    add templates for observation_selection 'historic_same_selection' and 'historic_similar_selection'

---
+++ sfgad/modules/observation_selection/historic_similar_selection.py
@@ -0,0 +1,26 @@
+from .observation_selection import ObservationSelection
+
+
+class HistoricSimilarSelection(ObservationSelection):
+    """
+    This observation selection gathers all historic observations of all recorded vertices of the same type.
+    The results can be limited by providing a limit parameter.
+    """
+
+    def __init__(self, limit=None):
+        # check whether limit is given and set self.limit if thats the case
+        if limit is not None:
+            if not isinstance(limit, int) or not limit >= 1:
+                raise ValueError("The given parameter 'limit' should be an integer and >= 1!")
+
+        self.limit = limit
+
+    def gather(self, vertex_name, database):
+        """
+        Takes a vertex and a reference to the database.
+        Returns a dataframe of all the relevant vertices that are needed for calculating p_value of the vertex.
+        :param vertex_name: The name of a vertex
+        :param database: The reference to the Database
+        :return: Dataframe of the relevant entries in the database
+        """
+        # TODO: implement the function
