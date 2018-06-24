6330bb180f83bf509f49d9a45e6bacbddeb6b828
Alex Rubinshteyn <alexander.rubinshteyn@student.kit.edu>
2018-06-24 15:05:10 +0200

    add some more tests for hotspot_features

--- sfgad/test/feature/test_hotspot_features.py
+++ sfgad/test/feature/test_hotspot_features.py
@@ -4,6 +4,8 @@ import numpy as np
 
 from sfgad.modules.feature.hotspot_features import HotSpotFeatures
 from collections import deque
+from pandas.util.testing import assert_frame_equal, assert_series_equal
+
 
 class TestHotSpotFeatures(TestCase):
 
@@ -39,6 +41,21 @@ class TestHotSpotFeatures(TestCase):
         self.decay_lambda = 1 / (self.half_life * self.window_size)
 
         self.hotspot_features = HotSpotFeatures(half_life=self.half_life, window_size=self.window_size)
+
+        # the target output of the feature after 1. (2.) time step
+        self.target_df_1 = pd.DataFrame(data={'name': ['A', 'B', 'C'], 'CorrelationChange': [np.nan, np.nan, np.nan],
+                                              'MagnitudeChange': [np.nan, np.nan, np.nan]},
+                                        columns=['name', 'CorrelationChange', 'MagnitudeChange'])
+        self.target_df_2 = pd.DataFrame(data={'name': ['A', 'B', 'C'], 'CorrelationChange': [1.110223e-16, 0, 0],
+                                              'MagnitudeChange': [-12.071068, -9.656854, -2.414214]},
+                                        columns=['name', 'CorrelationChange', 'MagnitudeChange'])
+        self.target_df_3 = pd.DataFrame(data={'name': ['A', 'B', 'C', 'D', 'E', 'F'],
+                                              'CorrelationChange': [1.110223e-16, 0, 1.562279e-01,
+                                                                    np.nan, np.nan, np.nan],
+                                              'MagnitudeChange': [-27.892136, -22.313708, -8.010025,
+                                                                  np.nan, np.nan, np.nan]},
+                                        columns=['name', 'CorrelationChange', 'MagnitudeChange'])
+
 
     def test_init(self):
         self.assertEqual(self.hotspot_features.names, ["CorrelationChange", "MagnitudeChange"])
@@ -216,27 +233,157 @@ class TestHotSpotFeatures(TestCase):
         self.assertEqual(self.hotspot_features.prev_timestamps, deque([pd.Timestamp('2018-01-01 00:00:10'),
                                                                        pd.Timestamp('2018-01-01 00:00:13')]))
 
+    def test_result_df_shape(self):
+        result_df_1 = self.hotspot_features.process_vertices(df_edges=self.df_1, n_jobs=1)
+        result_df_2 = self.hotspot_features.process_vertices(df_edges=self.df_2, n_jobs=1)
+        result_df_3 = self.hotspot_features.process_vertices(df_edges=self.df_3, n_jobs=1)
+
+        self.assertEqual(result_df_1.shape, self.target_df_1.shape)
+        self.assertEqual(result_df_2.shape, self.target_df_2.shape)
+        self.assertEqual(result_df_3.shape, self.target_df_3.shape)
+
+    def test_result_df_columns(self):
+        result_df_1 = self.hotspot_features.process_vertices(df_edges=self.df_1, n_jobs=1)
+
+        self.assertEqual(result_df_1.columns.tolist(), ['name', 'CorrelationChange', 'MagnitudeChange'])
+
+    def test_result_df_dtypes(self):
+        result_df_1 = self.hotspot_features.process_vertices(df_edges=self.df_1, n_jobs=1)
+        result_df_2 = self.hotspot_features.process_vertices(df_edges=self.df_2, n_jobs=1)
+        result_df_3 = self.hotspot_features.process_vertices(df_edges=self.df_3, n_jobs=1)
+
+        assert_series_equal(result_df_1.dtypes, self.target_df_1.dtypes)
+        assert_series_equal(result_df_2.dtypes, self.target_df_2.dtypes)
+        assert_series_equal(result_df_3.dtypes, self.target_df_3.dtypes)
+
+    def test_result_df_values(self):
+        result_df_1 = self.hotspot_features.process_vertices(df_edges=self.df_1, n_jobs=1)
+        result_df_2 = self.hotspot_features.process_vertices(df_edges=self.df_2, n_jobs=1)
+        result_df_3 = self.hotspot_features.process_vertices(df_edges=self.df_3, n_jobs=1)
+
+        assert_series_equal(result_df_1['name'], self.target_df_1['name'])
+        assert_series_equal(result_df_2['name'], self.target_df_2['name'])
+        assert_series_equal(result_df_3['name'], self.target_df_3['name'])
+        assert_series_equal(result_df_1['CorrelationChange'], self.target_df_1['CorrelationChange'])
+        assert_series_equal(result_df_2['CorrelationChange'], self.target_df_2['CorrelationChange'])
+        assert_series_equal(result_df_3['CorrelationChange'], self.target_df_3['CorrelationChange'])
+        assert_series_equal(result_df_1['MagnitudeChange'], self.target_df_1['MagnitudeChange'])
+        assert_series_equal(result_df_2['MagnitudeChange'], self.target_df_2['MagnitudeChange'])
+        assert_series_equal(result_df_3['MagnitudeChange'], self.target_df_3['MagnitudeChange'])
 
     def test_process_vertices(self):
-        self.fail()
+        #
+        # test_overall_processing_all_nodes
+        #
+        # test the calculation of hotspot features in the 1. time step ('df_1')
+        assert_frame_equal(self.hotspot_features.process_vertices(df_edges=self.df_1, n_jobs=1), self.target_df_1)
 
-    def test_compute_multiple_nodes(self):
-        self.fail()
+        # test the calculation of hotspot features in the 2. time step ('df_2')
+        assert_frame_equal(self.hotspot_features.process_vertices(df_edges=self.df_2, n_jobs=1), self.target_df_2)
+
+        # test the calculation of hotspot features in the 3. time step ('df_3')
+        assert_frame_equal(self.hotspot_features.process_vertices(df_edges=self.df_3, n_jobs=1), self.target_df_3)
+
+    def test_process_vertices_wrong_input(self):
+        self.hotspot_features.process_vertices(df_edges=self.df_1, n_jobs=1)
+
+        # assertion after getting old data
+        self.assertRaises(AssertionError, self.hotspot_features.process_vertices, self.df_1, 1)
 
     def test_compute(self):
-        self.fail()
+        # test compute method on the node 'A' in the 3. time step
+        self.hotspot_features.process_vertices(df_edges=self.df_1, n_jobs=1)
+        self.hotspot_features.process_vertices(df_edges=self.df_2, n_jobs=1)
+
+        # process 3. time step
+        self.hotspot_features.interpreter.interpret(self.df_3)
+
+        product_matrix, cor, mag, cor_change, mag_change = self.hotspot_features.compute(
+            'A', pd.Timestamp('2018-01-01 00:00:30'))
+
+        # check product matrix
+        np.testing.assert_allclose(product_matrix, np.array([[26.3137085, 13.15685425], [13.15685425, 6.57842712]]),
+                                   rtol=1e-5, atol=0)
+        # check correlation
+        self.assertEqual(cor, [-0.89442719099991586, -0.44721359549995793])
+
+        # check magnitude
+        self.assertEqual(mag, 32.892135623730951)
+
+        # check correlation change
+        self.assertEqual(cor_change, 1.1102230246251565e-16)
+
+        # check magnitude change
+        self.assertEqual(mag_change, -27.892135623730951)
 
     def test_calculate_product_matrix(self):
-        self.fail()
+        # test compute method on the node 'A' in the 3. time step
+        self.hotspot_features.process_vertices(df_edges=self.df_1, n_jobs=1)
+        self.hotspot_features.process_vertices(df_edges=self.df_2, n_jobs=1)
+
+        # process 3. time step
+        self.hotspot_features.interpreter.interpret(self.df_3)
+
+        product_matrix = self.hotspot_features.calculate_product_matrix(0, pd.Timestamp('2018-01-01 00:00:30'))
+
+        # check product matrix
+        np.testing.assert_allclose(product_matrix, np.array([[26.3137085, 13.15685425], [13.15685425, 6.57842712]]),
+                                   rtol=1e-5, atol=0)
 
     def test_consider_prev_product_matrix(self):
-        self.fail()
+        # test the consideration of product matrix of 'A' in the 3. time step
+        self.hotspot_features.process_vertices(df_edges=self.df_1, n_jobs=1)
+        self.hotspot_features.process_vertices(df_edges=self.df_2, n_jobs=1)
+
+        # process 3. time step
+        self.hotspot_features.interpreter.interpret(self.df_3)
+
+        updated_matrix = self.hotspot_features.consider_prev_product_matrix(0, pd.Timestamp('2018-01-01 00:00:30'), 2)
+
+        np.testing.assert_allclose(updated_matrix, np.array([[6.82842712, 3.41421356], [3.41421356, 1.70710678]]),
+                                   rtol=1e-5, atol=0)
 
     def test_reconstruct_edges(self):
-        self.fail()
+        self.hotspot_features.process_vertices(df_edges=self.df_1, n_jobs=1)
+        self.hotspot_features.process_vertices(df_edges=self.df_2, n_jobs=1)
+
+        # reconstruct edges of 'A' after 2. time step
+        self.assertEqual(self.hotspot_features.reconstruct_edges(0), [(0, 1), (0, 2)])
 
     def test_cal_matrix_features(self):
-        self.fail()
+        # test computation of correlation and magnitude of 'A' in the 3. time step
+        self.hotspot_features.process_vertices(df_edges=self.df_1, n_jobs=1)
+        self.hotspot_features.process_vertices(df_edges=self.df_2, n_jobs=1)
+
+        # process 3. time step
+        self.hotspot_features.interpreter.interpret(self.df_3)
+
+        product_matrix = self.hotspot_features.calculate_product_matrix(0, pd.Timestamp('2018-01-01 00:00:30'))
+
+        cor, mag = self.hotspot_features.cal_matrix_features(product_matrix)
+
+        # check correlation
+        self.assertEqual(cor, [-0.89442719099991586, -0.44721359549995793])
+
+        # check magnitude
+        self.assertEqual(mag, 32.892135623730951)
 
     def test_cal_activity_changes(self):
-        self.fail()
+        # test computation of correlation-change and magnitude-change of 'A' in the 3. time step
+        self.hotspot_features.process_vertices(df_edges=self.df_1, n_jobs=1)
+        self.hotspot_features.process_vertices(df_edges=self.df_2, n_jobs=1)
+
+        # process 3. time step
+        self.hotspot_features.interpreter.interpret(self.df_3)
+
+        product_matrix = self.hotspot_features.calculate_product_matrix(0, pd.Timestamp('2018-01-01 00:00:30'))
+
+        cor, mag = self.hotspot_features.cal_matrix_features(product_matrix)
+
+        cor_change, mag_change = self.hotspot_features.cal_activity_changes(0, cor, mag)
+
+        # check correlation change
+        self.assertEqual(cor_change, 1.1102230246251565e-16)
+
+        # check magnitude change
+        self.assertEqual(mag_change, -27.892135623730951)
