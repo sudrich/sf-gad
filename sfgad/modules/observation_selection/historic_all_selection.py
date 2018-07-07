721240589de3b2f69d4607650174f97736b72aaa
Alex Rubinshteyn <alexander.rubinshteyn@student.kit.edu>
2018-07-07 15:55:41 +0200

    add HistoricAllSelection

---
+++ sfgad/modules/observation_selection/historic_all_selection.py
@@ -0,0 +1,27 @@
+from .observation_selection import ObservationSelection
+
+
+class HistoricAllSelection(ObservationSelection):
+    """
+    This observation selection gathers all historic observations of all vertices.
+    The results can be limited by providing a limit parameter.
+    """
+
+    def __init__(self, limit=None):
+        # check whether limit is given and set self.limit if thats the case
+        if limit is not None:
+            if not isinstance(limit, int) or not limit >= 1:
+                raise ValueError("The given parameter 'limit' should be an integer and >= 1!")
+
+        self.limit = limit
+
+    def gather(self, vertex_name, vertex_type, database):
+        """
+        Takes a vertex and a reference to the database.
+        Returns a dataframe of all the relevant vertices that are needed for calculating p_value of the vertex.
+        :param vertex_name: The name of a vertex
+        :param vertex_type: The type of the vertex
+        :param database: The reference to the Database
+        :return: Dataframe of the relevant entries in the database
+        """
+        return database.select_all().head(self.limit)
