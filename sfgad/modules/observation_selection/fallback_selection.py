2c985ce39d7618890d3f7652fcd755ad6f8f35fe
Alex Rubinshteyn <alexander.rubinshteyn@student.kit.edu>
2018-07-07 15:55:29 +0200

    add FallbackSelection

---
+++ sfgad/modules/observation_selection/fallback_selection.py
@@ -0,0 +1,49 @@
+from .observation_selection import ObservationSelection
+
+
+class FallbackSelection(ObservationSelection):
+    """
+    This observation selection includes 2 selection rules. If the first selection rule fails to provide enough
+    observations, this selection falls back on a secondary rule.
+    """
+
+    def __init__(self, first_rule, second_rule, threshold, limit=None):
+        # check whether limit is given and set self.limit if thats the case
+        if not isinstance(threshold, int) or not threshold >= 1:
+            raise ValueError("The given parameter 'threshold' should be an integer and >= 1!")
+
+        # check whether limit is given and whether its greater than threshold
+        if limit is not None:
+            if not isinstance(limit, int) or not limit >= 1:
+                raise ValueError("The given parameter 'limit' should be an integer and >= 1!")
+            if limit <= threshold:
+                raise ValueError("The given parameter 'limit' should be greater than the given parameter 'threshold'")
+
+        self.first_rule = first_rule
+        self.second_rule = second_rule
+        self.threshold = threshold
+        self.limit = limit
+
+    def gather(self, vertex_name, vertex_type, database):
+        """
+        Takes a vertex and a reference to the database.
+        Returns a dataframe of all the relevant vertices that are needed for calculating p_value of the vertex.
+        :param vertex_name: The name of a vertex
+        :param vertex_type: The type of the vertex
+        :param database: The reference to the Database
+        :return: Dataframe of the relevant entries in the database
+        """
+        # get the results from the first rule
+        results = self.first_rule.gather(vertex_name, vertex_type, database)
+
+        # get and append the results from the second rule if the first rule fails to provide enough observations
+        if results.shape[0] < self.threshold:
+            results_second_rule = self.second_rule.gather(vertex_name, vertex_type, database)
+            results = results.append(results_second_rule, ignore_index=True)
+
+        #TODO: Filtern von gleichen Einträgen
+
+        return results.head(self.limit)
+
+
+
