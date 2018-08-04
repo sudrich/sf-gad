Fixed a bug with type specific weighting that prevented a mapping from types to weights.

sfgad/modules/weighting/type_specific_weight.py
@@ -27,6 +27,6 @@ class TypeSpecificWeight(Weighting):
 
         reference_feature_values.fillna(0)
         weight_df = pd.DataFrame(reference_feature_values['time_window'], columns=['time_window'], dtype=np.int)
-        weight_df['weight'] = reference_feature_values['type']
+        weight_df['weight'] = reference_feature_values['type'].replace(self.type_dict)
         # weight_df['weight'] = weight_df['weight'].clip(lower=0.0)
         return weight_df