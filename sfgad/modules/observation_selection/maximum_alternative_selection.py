d42ef5ce0c569db8c1d24a9be6f1b7c54abd4bae
Alex Rubinshteyn <alexander.rubinshteyn@student.kit.edu>
2018-08-05 21:10:27 +0200

    add maximum_alternative_selection with tests

--- sfgad/modules/observation_selection/maximum_alternative_selection.py
+++ sfgad/modules/observation_selection/maximum_alternative_selection.py
@@ -1,28 +1,21 @@
 from .observation_selection import ObservationSelection
 
 
-class AlternativeSelection(ObservationSelection):
+class MaximumAlternativeSelection(ObservationSelection):
     """
-    This observation selection includes 2 selection rules. If the first selection rule fails to provide enough
-    observations, this selection falls back on a secondary rule and returns only the observations of the second
-    selection rule.
+    This observation selection includes 2 selection rules. This selection apply both rules and returns the result with
+    more observations.
     """
 
-    def __init__(self, first_rule, second_rule, threshold, limit=None):
-        # check whether limit is given and set self.limit if this is the case
-        if not isinstance(threshold, int) or not threshold >= 1:
-            raise ValueError("The given parameter 'threshold' should be an integer and >= 1!")
+    def __init__(self, first_rule, second_rule, limit=None):
 
         # check whether limit is given and whether its greater than threshold
         if limit is not None:
             if not isinstance(limit, int) or not limit >= 1:
                 raise ValueError("The given parameter 'limit' should be an integer and >= 1!")
-            if limit < threshold:
-                raise ValueError("The given parameter 'limit' should be greater than the given parameter 'threshold'")
 
         self.first_rule = first_rule
         self.second_rule = second_rule
-        self.threshold = threshold
         self.limit = limit
 
     def gather(self, vertex_name, vertex_type, database):
@@ -35,12 +28,16 @@ class AlternativeSelection(ObservationSelection):
         :return: Dataframe of the relevant entries in the database
         """
         # get the results from the first rule
-        result = self.first_rule.gather(vertex_name, vertex_type, database)
+        result_first_rule = self.first_rule.gather(vertex_name, vertex_type, database)
 
-        # get the results from the second rule, if the first rule fails to provide enough observations
-        if result.shape[0] < self.threshold:
-            result_second_rule = self.second_rule.gather(vertex_name, vertex_type, database)
+        # get the results from the second rule
+        result_second_rule = self.second_rule.gather(vertex_name, vertex_type, database)
+
+        # decide which rule yielded more observations
+        if result_second_rule.shape[0] > result_first_rule.shape[0]:
             result = result_second_rule
+        else:
+            result = result_first_rule
 
         if self.limit is not None:
             result = result.head(self.limit)
