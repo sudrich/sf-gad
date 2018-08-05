580d0bdce546afb1941532dd37fc79c0852a7415
Alex Rubinshteyn <alexander.rubinshteyn@student.kit.edu>
2018-08-05 16:29:28 +0200

    add test for historic same selection rule

--- sfgad/test/observation_selection/test_historic_same_selection.py
+++ sfgad/test/observation_selection/test_historic_same_selection.py
@@ -1,10 +1,44 @@
+import pandas as pd
+
 from unittest import TestCase
+from pandas.util.testing import assert_frame_equal, assert_series_equal
+
+from sfgad.modules.observation_selection.helper.database import Database
 from sfgad.modules.observation_selection.historic_same_selection import HistoricSameSelection
 
 
 class TestHistoricSameSelection(TestCase):
 
-    def test_gather(self):
-        self.fail()
+    def setUp(self):
+        # establish a connection to the database
+        self.db = Database(user='root', password='root', host='localhost', database='sfgad', table_name='historic_data',
+                           feature_names=['feature_A', 'feature_B'])
+        self.db.insert_record('Vertex_A', 'PERSON', 1, [24, 42])
+        self.db.insert_record('Vertex_B', 'PERSON', 1, [124, 142])
+        self.db.insert_record('Vertex_C', 'PICTURE', 1, [224, 242])
+        self.db.insert_record('Vertex_D', 'POST', 1, [324, 342])
+        self.db.insert_record('Vertex_A', 'PERSON', 2, [12, 24])
 
-    # TODO: implement some tests
+        # init a selection rule
+        self.sel_rule = HistoricSameSelection()
+
+    def tearDown(self):
+        # close db connection
+        self.db.close_connection()
+
+    def test_gather(self):
+        target_df = pd.DataFrame(data={'name': ['Vertex_A', 'Vertex_A'], 'type': ['PERSON', 'PERSON'],
+                                       'time_window': [1, 2], 'feature_A': [24.0, 12.0],
+                                       'feature_B': [42.0, 24.0]},
+                                 columns=['name', 'type', 'time_window', 'feature_A', 'feature_B'])
+
+        assert_frame_equal(self.sel_rule.gather('Vertex_A', 'PERSON', self.db), target_df)
+
+    def test_gather_with_limit(self):
+        target_df = pd.DataFrame(data={'name': ['Vertex_A'], 'type': ['PERSON'],
+                                       'time_window': [1], 'feature_A': [24.0],
+                                       'feature_B': [42.0]},
+                                 columns=['name', 'type', 'time_window', 'feature_A', 'feature_B'])
+
+        self.sel_rule = HistoricSameSelection(limit=1)
+        assert_frame_equal(self.sel_rule.gather('Vertex_A', 'PERSON', self.db), target_df)
