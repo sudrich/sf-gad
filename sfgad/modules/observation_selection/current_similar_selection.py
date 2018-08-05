44611636d5f01f6988be0f5c106cb1b431a99d4d
Alex Rubinshteyn <alexander.rubinshteyn@student.kit.edu>
2018-08-06 00:00:41 +0200

    add curren_similar_selection rule + tests

---
+++ sfgad/modules/observation_selection/current_similar_selection.py
@@ -0,0 +1,38 @@
+from .observation_selection import ObservationSelection
+
+
+class CurrentSimilarSelection(ObservationSelection):
+    """
+    This observation selection gathers all current observations of all vertices of the same type.
+    The results can be limited by providing a limit parameter.
+    """
+
+    def __init__(self, limit=None):
+        # check whether limit is given and set self.limit if thats the case
+        if limit is not None:
+            if not isinstance(limit, int) or not limit >= 1:
+                raise ValueError("The given parameter 'limit' should be an integer and >= 1!")
+
+        self.limit = limit
+
+    def gather(self, vertex_name, vertex_type, current_time_window, database):
+        """
+        Takes a vertex and a reference to the database.
+        Returns a dataframe of all the relevant vertices that are needed for calculating p_value of the vertex.
+        :param vertex_name: The name of the vertex
+        :param vertex_type: The type of the vertex
+        :param current_time_window: The current time step
+        :param database: The reference to the Database
+        :return: Dataframe of the relevant entries in the database
+        """
+
+        result = database.select_by_time_step(current_time_window)
+
+        # filter rows by vertex_type
+        result = result[result['type'] == vertex_type]
+        result = result.reset_index(drop=True)
+
+        if self.limit is not None:
+            result = result.head(self.limit)
+
+        return result
---
+++ sfgad/test/observation_selection/test_current_similar_selection.py
@@ -0,0 +1,46 @@
+import pandas as pd
+
+from unittest import TestCase
+from pandas.util.testing import assert_frame_equal
+
+from sfgad.modules.observation_selection.helper.database import Database
+from sfgad.modules.observation_selection.current_similar_selection import CurrentSimilarSelection
+
+
+class TestCurrentSimilarSelection(TestCase):
+
+    def setUp(self):
+        # establish a connection to the database
+        self.db = Database(user='root', password='root', host='localhost', database='sfgad', table_name='historic_data',
+                           feature_names=['feature_A', 'feature_B'])
+        self.db.insert_record('Vertex_A', 'PERSON', 1, [24, 42])
+        self.db.insert_record('Vertex_B', 'PERSON', 1, [124, 142])
+        self.db.insert_record('Vertex_C', 'PICTURE', 1, [224, 242])
+        self.db.insert_record('Vertex_D', 'POST', 1, [324, 342])
+        self.db.insert_record('Vertex_A', 'PERSON', 2, [12, 24])
+        self.db.insert_record('Vertex_C', 'PERSON', 2, [11, 22])
+
+        # init a selection rule
+        self.sel_rule = CurrentSimilarSelection()
+
+    def tearDown(self):
+        # close db connection
+        self.db.close_connection()
+
+    def test_gather(self):
+        target_df = pd.DataFrame(data={'name': ['Vertex_A', 'Vertex_C'],
+                                       'type': ['PERSON', 'PERSON'],
+                                       'time_window': [2, 2], 'feature_A': [12.0, 11.0],
+                                       'feature_B': [24.0, 22.0]},
+                                 columns=['name', 'type', 'time_window', 'feature_A', 'feature_B'])
+
+        assert_frame_equal(self.sel_rule.gather(None, 'PERSON', 2, self.db), target_df)
+
+    def test_gather_with_limit(self):
+        target_df = pd.DataFrame(data={'name': ['Vertex_A'], 'type': ['PERSON'],
+                                       'time_window': [2], 'feature_A': [12.0],
+                                       'feature_B': [24.0]},
+                                 columns=['name', 'type', 'time_window', 'feature_A', 'feature_B'])
+
+        self.sel_rule = CurrentSimilarSelection(limit=1)
+        assert_frame_equal(self.sel_rule.gather(None, 'PERSON', 2, self.db), target_df)
