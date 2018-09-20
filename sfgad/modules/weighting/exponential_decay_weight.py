efcaa7430725ca8973cac49b67e71247c109b291
Simon Sudrich <uzcyg@student.kit.edu>
2018-09-20 23:43:41 +0200

    Changed the definition of the decay lambda for the exponential decay weights to be in line with the literature definition.

--- sfgad/modules/weighting/exponential_decay_weight.py
+++ sfgad/modules/weighting/exponential_decay_weight.py
@@ -13,10 +13,10 @@ class ExponentialDecayWeight(Weighting):
     """
 
     def __init__(self, half_life):
-        self.decay_lambda = math.log(0.5) / half_life
+        self.decay_lambda = math.log(2) / half_life
 
     def compute(self, reference_meta_info, current_meta_info):
         check_meta_info_dataframe(reference_meta_info, required_columns=['time_window'])
         check_meta_info_series(current_meta_info, required_columns=['time_window'])
 
-        return np.exp(-1 * self.decay_lambda * (reference_meta_info['time_window'] - current_meta_info['time_window']))
+        return np.exp(self.decay_lambda * (reference_meta_info['time_window'] - current_meta_info['time_window']))
